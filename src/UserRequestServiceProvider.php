1c653d0491856273e734ad7a9da9da35f72e76b7
Bhaktij Koli <bhaktijkoli121@gmail.com>
2020-01-24 19:40:40 +0530

    Initial commit

---
+++ src/UserRequestServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BhaktijKoli\UserRequest;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserRequestServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+      
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
